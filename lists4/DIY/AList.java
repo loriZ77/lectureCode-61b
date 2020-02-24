d1dd6dc8f8ee6ebcb86f5f658cf3a6c1a437defc
Lori <57802869+loriZ77@users.noreply.github.com>
2020-02-24 23:22:25 +0800

    1st attempt: the naive array based list

--- lists4/DIY/AList.java
+++ lists4/DIY/AList.java
@@ -1,33 +1,53 @@
 /** Array based list.
  *  @author Josh Hug
  */
+//         0 1 2 3 4 5 6
+// items: [4,7,3,6,0,0,0,.....]
+
+/**
+ * Invariants:
+ * addLast: the next item we want to add, will go into position size;
+ * getLast: the item we want to return is in position size-1;
+ * size: the number of items in the list should be size;
+ */
 
 public class AList {
+    int[] items;
+    int size;
+
     /** Creates an empty list. */
     public AList() {
+        items=new int[100];
+        size=0;
     }
 
     /** Inserts X into the back of the list. */
     public void addLast(int x) {
+        items[size]=x;
+        size=size+1;
+
     }
 
     /** Returns the item from the back of the list. */
     public int getLast() {
-        return 0;        
+        return items[size-1];
     }
     /** Gets the ith item in the list (0 is the front). */
     public int get(int i) {
-        return 0;        
+        return items[i];
     }
 
     /** Returns the number of items in the list. */
     public int size() {
-        return 0;        
+        return size;
     }
 
     /** Deletes item from back of the list and
       * returns deleted item. */
     public int removeLast() {
-        return 0;
+        int deletedItem=getLast();
+        items[size-1]=0;
+        size=size-1;
+        return deletedItem;
     }
 } 
